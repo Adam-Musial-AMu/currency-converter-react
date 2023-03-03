b8ccb9e0675f98ad38e28ea841119f9884462254
Adam Musial <adammusial@wp.pl>
2023-03-03 19:54:20 +0100

    moving label text to the Select component

--- src/Select/index.js
+++ src/Select/index.js
@@ -2,15 +2,22 @@ import "./style.css";
 
 
 const Select = ({ currencies }) => {
-return (
-    <select className="form__field">
-        {currencies.map(currency =>
-            <option className="form__option" key={currency.id}>
-                {currency.name}
-            </option>)}
-    </select>
-)
-    
+    return (
+        <p className="form__paragraph">
+            <label className="form__label">
+                <span className="form__labelText">
+                    Waluta*
+                </span>
+                <select className="form__field">
+                    {currencies.map(currency =>
+                        <option className="form__option" key={currency.id}>
+                            {currency.name}
+                        </option>)}
+                </select>
+            </label>
+        </p>
+    )
+
 };
 
 export default Select;
