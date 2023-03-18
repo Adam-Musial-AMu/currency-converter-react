43fc0ca32efa4b7c93ea1179151b80eab8a38750
Adam Musial <adammusial@wp.pl>
2023-03-18 19:51:30 +0100

    currencies moved to a separate file

---
+++ src/currencies.js
@@ -0,0 +1,6 @@
+export const currencies =
+    [
+        { id: 1, name: "Dolar amerykański", symbol: "USD", rate: 4.47 },
+        { id: 2, name: "Funt brytyjski", symbol: "GBP", rate: 5.34 },
+        { id: 3, name: "Euro", symbol: "EUR", rate: 4.72 },
+    ];
