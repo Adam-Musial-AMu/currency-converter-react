32bab4e27b558858f0fc46dd278886f33a3a89ad
Adam Musial <adammusial@wp.pl>
2023-03-28 19:16:32 +0200

    code simplification

--- src/Time/index.js
+++ src/Time/index.js
@@ -1,17 +1,13 @@
 import "./style.css";
 
-
-const Time = ({ currentDate }) => {
-
-    return (
-        <p className="time">
-            Dzisiaj jest&nbsp;
-            {currentDate.toLocaleString(
-                "pl", { month: "long", weekday: "long", day: "numeric" }
-            )},&nbsp;
-            {currentDate.toLocaleTimeString()}
-        </p>
-    )
-};
+const Time = ({ currentDate }) => (
+    <p className="time">
+        Dzisiaj jest&nbsp;
+        {currentDate.toLocaleString(
+            "pl", { month: "long", weekday: "long", day: "numeric" }
+        )},&nbsp;
+        {currentDate.toLocaleTimeString()}
+    </p>
+);
 
 export default Time;
