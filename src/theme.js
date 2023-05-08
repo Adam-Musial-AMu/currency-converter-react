011491a18e46c9f7aec4b4497fb9f7a1084d0d26
Adam Musial <adammusial@wp.pl>
2023-05-08 20:09:16 +0200

    theme created

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        white: "#fff",
+        emperor: "#555",
+        teal: "#008080",
+        aliceBlue: "#f0f8ff",
+        darkGreen: "#013220",
+        axolotl: "#466d46",  
+    }
+};
