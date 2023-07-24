4e3852163836671e5cc6a51fefdb8a3d5d15f337
Adam Musial <adammusial@wp.pl>
2023-07-24 21:48:04 +0200

    passing props to currencySelect, adding messages

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,73 +1,67 @@
+import { useState } from "react";
 import CurrencyDate from "../CurrencyDate";
 import CurrencySelect from "../CurrencySelect";
 import Result from "../Result";
 import Time from "../Time";
-import { currencies } from "../currencies";
 import { StyledForm, Fieldset, Legend, Data, Label, Text, Input, Button } from "./styled";
-import { useState } from "react";
 
-const Form = ({ calculateResult, result, legendText, buttonText }) => {
-    const [currency, setCurrency] = useState(currencies[0].symbol);
-    const [amount, setAmount] = useState("");
+const Form = ({ calculateResult, result, legendText, buttonText, date, loading, error, currencyData, currencyOptions }) => {
+  const [currency, setCurrency] = useState("");
+  const [amount, setAmount] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(currency, amount);
-    }
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(amount, currency);
+  };
 
-    return (
-        <main>
-            <StyledForm onSubmit={onFormSubmit}>
-                <Fieldset>
-                    <Legend>
-                        {legendText}
-                    </Legend>
-                    <Time />
-                    <Data>
-                        <Label>
-                            <Text>
-                                Kwota w PLN*
-                            </Text>
-                            <Input
-                                name="amountPln"
-                                type="number"
-                                min="1"
-                                placeholder="Wpisz kwotę w PLN"
-                                value={amount}
-                                onChange={(event) => setAmount(event.target.value)}
-                                required />
-                        </Label>
-                    </Data>
-                    <Data>
-                        <Label>
-                            <Text>
-                                Waluta*
-                            </Text>
-                            <CurrencySelect setCurrency = {setCurrency}/>
-                           {/* <Input as="select"
-                                value={currency}
-                                onChange={({ target }) => setCurrency(target.value)}>
-                                {currencies.map((currency) => (
-                                    <option
-                                        key={currency.symbol}
-                                        value={currency.symbol}>
-                                        {currency.name}
-                                    </option>
-                                ))}
-                                </Input> */}
-                        </Label>
-                    </Data>
-                    <p>
-                        <Button>
-                            {buttonText}
-                        </Button>
-                    </p>
-                    <CurrencyDate />
-                    <Result result={result} />
-                </Fieldset>
-            </StyledForm>
-        </main>
-    )
+  if (loading) {
+    return <p>Chwileczkę... ładujemy...</p>;
+  }
+
+  if (error) {
+    return <p>Coś poszło nie tak... Proszę spróbować ponownie później.</p>;
+  }
+
+  if (!currencyData) {
+    return null;
+  }
+
+
+  return (
+    <main>
+      <StyledForm onSubmit={onFormSubmit}>
+        <Fieldset>
+          <Legend>{legendText}</Legend>
+          <Time />
+          <Data>
+            <Label>
+              <Text>Kwota w PLN*</Text>
+              <Input
+                name="amountPln"
+                type="number"
+                min="1"
+                placeholder="Wpisz kwotę w PLN"
+                value={amount}
+                onChange={(event) => setAmount(event.target.value)}
+                required
+              />
+            </Label>
+          </Data>
+          <Data>
+            <Label>
+              <Text>Waluta*</Text>
+              <CurrencySelect currencyOptions={currencyOptions} currencyData={currencyData} setCurrency={setCurrency} />
+            </Label>
+          </Data>
+          <div>
+            <Button>{buttonText}</Button>
+          </div>
+          <CurrencyDate date={date} />
+          <Result result={result} />
+        </Fieldset>
+      </StyledForm>
+    </main>
+  );
 };
 
 export default Form;
